10e7a9fe876dbda1a7dc9c4edcaa6f90a6495f8f
Chad Eliason <cmestuff@gmail.com>
2024-10-17 18:53:15 -0500

    Updates models, new data

--- src/scrape.py
+++ src/scrape.py
@@ -156,3 +156,82 @@ metadata_df.to_csv('../data/interim/rf_meta.csv', index=False)
 # unique_addresses.index('816 S MAPLE AVE APT 2S')
 
 # idx = range(2867, len(unique_addresses))  # subset of addresses - "debugging mode"
+
+
+
+import pandas as pd
+rf_metadata = pd.read_csv('../data/interim/rf_meta.csv')
+rf_metadata['property_id'] = rf_metadata['property_id'].fillna(-1).astype(int)
+rf_metadata
+
+
+url=rf_metadata['search_url'][i]
+pid=rf_metadata['property_id'][i]
+
+# Function to get metadata from redfin
+def get_year(url: str, pid: int):
+    import requests
+    import json
+    import re
+    from redfin import Redfin
+    client = Redfin()
+    page = requests.get('https://www.redfin.com/stingray/api/home/details/mainHouseInfoPanelInfo?propertyId=' + str(pid) + '&accessLevel=3&path=' + url, headers={'user-agent': 'redfin'})
+    info = json.loads(page.text[4:])
+    content = info['payload']['mainHouseInfo']['selectedAmenities'][1]['content']
+    home_type = info['payload']['mainHouseInfo']['selectedAmenities'][3]['content']
+    year_built = re.findall('Built in (\\d+)', content)[0]
+    lot_size = re.findall('([0-9,]+)', info['payload']['mainHouseInfo']['selectedAmenities'][2]['content'])
+    lot_size = float(lot_size[0].replace(',', ''))
+    # Further data using redfin scraper
+    result = client.page_tags(url)
+    try:
+        price = next((x for x in result['payload']['metaTags'] if x['name'] == 'twitter:text:price'), None)['content']
+    except:
+        price = float('nan')
+    sqft = next((x for x in result['payload']['metaTags'] if x['name'] == 'twitter:text:sqft'), None)['content']
+    beds = next((x for x in result['payload']['metaTags'] if x['name'] == 'twitter:text:beds'), None)['content']
+    baths = next((x for x in result['payload']['metaTags'] if x['name'] == 'twitter:text:baths'), None)['content']
+
+    return home_type, year_built, lot_size, price, beds, baths, sqft
+
+# Testing zone
+url='/IL/Oak-Park/1019-Highland-Ave-60304/home/13251221'
+pid=13251221
+# page = requests.get('https://www.redfin.com/stingray/api/home/details/mainHouseInfoPanelInfo?propertyId=' + str(pid) + '&accessLevel=3&path=' + url, headers={'user-agent': 'redfin'})
+# info = json.loads(page.text[4:])
+get_year(url, pid)
+
+
+
+# Add home type and year built to the existing dataframe:
+from tqdm.notebook import tqdm
+from time import sleep
+
+for i in tqdm(range(len(rf_metadata))):
+    try:
+        subdat = get_year(rf_metadata['search_url'][i], rf_metadata['property_id'][i])
+        rf_metadata.loc[i, ['home_type', 'year_built', 'lot_size', 'price', 'beds', 'baths', 'sqft']] = subdat
+    except:
+        pass
+
+# Save intermediate file
+rf_metadata.head(50)
+rf_metadata.columns
+
+# Convert price to numeric
+rf_metadata['price'] = rf_metadata['price'].str.replace('$', '').str.replace(',', '').astype(float)
+
+# Convert beds to integer
+rf_metadata['beds'] = rf_metadata['beds'].str.replace('BR', '').astype(float)
+
+# Convert baths to numeric
+rf_metadata['baths'] = rf_metadata['baths'].str.replace('BA', '').astype(float)
+
+# Convert sqft to numeric
+rf_metadata.loc[rf_metadata['sqft'] == '-', 'sqft'] = float('nan')
+rf_metadata['sqft'] = rf_metadata['sqft'].str.replace(',', '').astype(float)
+
+rf_metadata.reset_index(drop=True, inplace=True)
+
+rf_metadata.to_csv('../data/interim/rf_meta_year_updated.csv', index=False)
+
